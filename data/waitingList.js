b1ab6c8620c1db90a54353f143de23f454c12e9d
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-29 16:32:35 -0500

    Setup the array object waitList

---
+++ data/waitingList.js
@@ -0,0 +1,12 @@
+// Adding waitingList data
+
+var waitList = [
+    {
+        customerName: "janedoe",
+        customerEmail: "janedoe@example.com",
+        customerID: "doejane12",
+        phoneNumber: "000-000-0000"    
+    }
+];
+
+module.exports = waitList;
