5c210b072881f13af99e53f843c2c7a13cd5ca18
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-29 16:38:57 -0500

    Getting routes for api calls

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,3 +7,13 @@
 var tableData = require("../data/tableList");
 var waitListData = require("../data/waitingList");
 
+module.exports = function(app) {
+
+    app.get('/api/tables', function(req, res) {
+        res.json(tableData);
+    });
+
+    app.get('/api/waitlist', function(req, res) {
+        res.json(waitListData);
+    });
+}
