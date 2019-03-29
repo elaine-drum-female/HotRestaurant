5b8018d3146958e8024ca5898463345575c5e1b9
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-29 16:31:22 -0500

    Added the array object tableList

---
+++ data/tableList.js
@@ -0,0 +1,12 @@
+// Adding tableList data
+
+var tableList = [
+    {
+        customerName: "johndoe",
+        customerEmail: "johndoe@example.com",
+        customerID: "doejohn89",
+        phoneNumber: "000-000-0000"    
+    }
+];
+
+module.exports = tableList;
