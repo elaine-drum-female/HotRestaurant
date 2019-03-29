5ae29f2db156aeee489d1a8c6dbb7de5472ad770
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-29 16:27:05 -0500

    Included express dependency and created express server

---
+++ server.js
@@ -0,0 +1,15 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
